85768f8b284345ab0b54b42489b59b9c0836cae0
longzhenren <longzhenren@amur.com>
2022-10-04 23:37:41 +0800

    Add Home Web Controller;Finish User Service Impl

---
+++ home-service/home-user/src/main/java/com/amur/home/user/controller/UnityController.java
@@ -0,0 +1,61 @@
+package com.amur.home.user.controller;
+
+import com.amur.home.user.entity.UserUnity;
+import com.amur.home.user.service.UserUnityService;
+import com.amur.home.util.ResponseWrapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@Tag(name = "用户Unity信息")
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("/unity")
+public class UnityController {
+
+    @Resource
+    private UserUnityService userUnityService;
+
+    @GetMapping("/user")
+    @Operation(summary = "根据ID查询用户信息")
+    public ResponseWrapper<UserUnity> getUserUnityById(Long id) {
+        return ResponseWrapper.data(userUnityService.getById(id));
+    }
+
+    @PostMapping("/update/text")
+    @Operation(summary = "根据ID更新用户信息")
+    public ResponseWrapper<?> updateText(Long id, String text) {
+        UserUnity userUnity = userUnityService.getById(id);
+        userUnity.setText(text);
+        return ResponseWrapper.status(userUnityService.updateById(userUnity));
+    }
+
+    @GetMapping("/text")
+    @Operation(summary = "根据ID查询用户信息")
+    public ResponseWrapper<String> getText(Long id) {
+        return ResponseWrapper.data(userUnityService.getById(id).getText());
+    }
+
+    @GetMapping("/model")
+    @Operation(summary = "根据ID查询用户信息")
+    public ResponseWrapper<Long> getModel(Long id) {
+        return ResponseWrapper.data(userUnityService.getById(id).getModelId());
+    }
+
+    @PostMapping("/update/model")
+    @Operation(summary = "根据ID更新用户信息")
+    public ResponseWrapper<?> updateUserUnityModel(Long id, Long modelId) {
+        UserUnity userUnity = userUnityService.getById(id);
+        userUnity.setModelId(modelId);
+        return ResponseWrapper.status(userUnityService.updateById(userUnity));
+    }
+
+}
