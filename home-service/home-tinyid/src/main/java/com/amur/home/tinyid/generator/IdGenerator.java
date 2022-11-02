bfb124081f01dc8eb870f50c6364e0b87f0d0b87
longzhenren <longzhenren@amur.com>
2022-11-02 16:36:37 +0800

    integrate didi tinyId as a microservice;TinyID DAO实现;TinyID ID生成器实现

---
+++ home-service/home-tinyid/src/main/java/com/amur/home/tinyid/generator/IdGenerator.java
@@ -0,0 +1,23 @@
+package com.amur.home.tinyid.generator;
+
+import java.util.List;
+
+/**
+ * @author du_imba
+ */
+public interface IdGenerator {
+    /**
+     * get next id
+     *
+     * @return
+     */
+    Long nextId();
+
+    /**
+     * get next id batch
+     *
+     * @param batchSize
+     * @return
+     */
+    List<Long> nextId(Integer batchSize);
+}
