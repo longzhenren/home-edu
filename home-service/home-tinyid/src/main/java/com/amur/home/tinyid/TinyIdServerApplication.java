9b4d983bc0d8214b730b63c3fd1b9640df21e547
longzhenren <longzhenren@amur.com>
2022-11-02 09:50:24 +0800

    TinyID entity设计;TinyID服务实现

---
+++ home-service/home-tinyid/src/main/java/com/amur/home/tinyid/TinyIdServerApplication.java
@@ -0,0 +1,37 @@
+package com.amur.home.tinyid;
+
+import io.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author du_imba
+ */
+@EnableAsync
+@SpringBootApplication(scanBasePackages = {"com.amur.home"})
+@EnableScheduling
+@EnableAutoDataSourceProxy
+@Slf4j
+public class TinyIdServerApplication {
+
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(TinyIdServerApplication.class, args);
+        Environment env = application.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "Remote: \thttp://{}:{}\n" +
+                        "----------------------------------------------------------",
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+    }
+}
