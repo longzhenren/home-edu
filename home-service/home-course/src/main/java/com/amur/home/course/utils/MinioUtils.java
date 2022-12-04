798cbb0f52cd1ee014bd3bdb84dadb1e374b16fc
longzhenren <longzhenren@amur.com>
2022-12-04 18:24:48 +0800

    添加课程服务实现;消息服务完善

---
+++ home-service/home-course/src/main/java/com/amur/home/course/utils/MinioUtils.java
@@ -0,0 +1,114 @@
+package com.amur.home.course.utils;
+
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import io.minio.*;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class MinioUtils {
+
+    @Value("${minio.bucketName}")
+    private String bucketName;
+
+    @Autowired
+    private MinioClient minioClient;
+
+    public void existBucket(String name) {
+        try {
+            boolean exists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(name).build());
+            if (!exists) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(name).build());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<String> upload(MultipartFile[] multipartFile) {
+        List<String> names = new ArrayList<>(multipartFile.length);
+        for (MultipartFile file : multipartFile) {
+            String fileName = file.getOriginalFilename();
+            String[] split = fileName.split("\\.");
+            if (split.length > 1) {
+                fileName = split[0] + "_" + System.currentTimeMillis() + "." + split[1];
+            } else {
+                fileName = fileName + System.currentTimeMillis();
+            }
+            InputStream in = null;
+            try {
+                in = file.getInputStream();
+                minioClient.putObject(PutObjectArgs.builder().bucket(bucketName).object(fileName).stream(in, in.available(), -1).contentType(file.getContentType()).build());
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            names.add(fileName);
+        }
+        return names;
+    }
+
+    public ResponseEntity<byte[]> download(String fileName) {
+        ResponseEntity<byte[]> responseEntity = null;
+        InputStream in = null;
+        ByteArrayOutputStream out = null;
+        try {
+            in = minioClient.getObject(GetObjectArgs.builder().bucket(bucketName).object(fileName).build());
+            out = new ByteArrayOutputStream();
+            IOUtils.copy(in, out);
+            //封装返回值
+            byte[] bytes = out.toByteArray();
+            HttpHeaders headers = new HttpHeaders();
+            try {
+                headers.add("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, Constants.UTF_8));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            headers.setContentLength(bytes.length);
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            headers.setAccessControlExposeHeaders(Arrays.asList("*"));
+            responseEntity = new ResponseEntity<byte[]>(bytes, headers, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return responseEntity;
+    }
+}
