9f30e1e3f078addbb77693ec138c6fc99d397f2f
龙镇人 <hljzhangzhibo@126.com>
2022-12-14 16:53:56 +0800

    Add Home Web Controller, Add Maven Docker Plugin support

--- home-service/home-user/src/main/java/com/amur/homeuser/controller/HomeController.java
+++ home-service/home-user/src/main/java/com/amur/homeuser/controller/HomeController.java
@@ -1,17 +1,54 @@
 package com.amur.homeuser.controller;
 
+import com.amur.home.util.ResponseWrapper;
+import com.amur.homeuser.dto.AddHomeImageDto;
+import com.amur.homeuser.entity.HomeEntity;
 import com.amur.homeuser.service.impl.HomeServiceImpl;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/home")
+@Slf4j
 public class HomeController {
     private HomeServiceImpl homeService;
-    @GetMapping("/getbaseinfo")
-    public String getBaseInfo(@RequestParam("homeid") Long homeId) {
-        return homeService.getHomeInfo(homeId).toString();
+    @GetMapping("/getinfo")
+    public ResponseWrapper<HomeEntity> getBaseInfo(@RequestParam("homeid") Long homeId) {
+        return ResponseWrapper.data(homeService.getHomeInfo(homeId));
     }
+    @GetMapping("/getlist")
+    public ResponseWrapper<List<HomeEntity>> getList() {
+        return ResponseWrapper.data(homeService.getHomeList());
+    }
+    @PostMapping("/create")
+    public ResponseWrapper<Long> createHome(HomeEntity homeEntity) {
+        return ResponseWrapper.data(homeService.createHome(homeEntity));
+    }
+    @PostMapping("/update")
+    public ResponseWrapper<Boolean> updateHome(HomeEntity homeEntity) {
+        return ResponseWrapper.data(homeService.updateHome(homeEntity));
+    }
+    @PostMapping("/delete")
+    public ResponseWrapper<Boolean> deleteHome(@RequestParam("homeid") Long homeId) {
+        return ResponseWrapper.data(homeService.deleteHome(homeId));
+    }
+    @PostMapping("/updateuser")
+    public ResponseWrapper<Boolean> updateHomeUser(@RequestParam("homeid") Long homeId, @RequestParam("userid") Long userId) {
+        return ResponseWrapper.data(homeService.updateHomeUser(homeId, userId));
+    }
+    @PostMapping("/deleteuser")
+    public ResponseWrapper<Boolean> deleteHomeUser(@RequestParam("homeid") Long homeId, @RequestParam("userid") Long userId) {
+        return ResponseWrapper.data(homeService.deleteHomeUser(homeId, userId));
+    }
+    @PostMapping("/setadmin")
+    public ResponseWrapper<Boolean> setHomeAdmin(@RequestParam("homeid") Long homeId, @RequestParam("userid") Long userId) {
+        return ResponseWrapper.data(homeService.setHomeAdmin(homeId, userId));
+    }
+    @PostMapping("/addimage")
+    public ResponseWrapper<Boolean> addHomeImage(@RequestBody AddHomeImageDto request) {
+        return ResponseWrapper.data(homeService.addHomeImage(request));
+    }
+
 }
