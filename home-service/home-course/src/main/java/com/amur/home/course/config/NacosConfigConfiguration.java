a7db2d2c904359650dbacb2434ce1009b62578f9
龙镇人 <hljzhangzhibo@126.com>
2022-12-22 15:39:53 +0800

    integrate skywalker, remove zipkin and sleuth

---
+++ home-service/home-course/src/main/java/com/amur/home/course/config/NacosConfigConfiguration.java
@@ -0,0 +1,10 @@
+package com.amur.home.course.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@Configuration
+public class NacosConfigConfiguration {
+}
---
+++ home-service/home-course/src/test/java/com/amur/home/course/HomeCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amur.home.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
