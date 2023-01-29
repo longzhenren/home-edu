1b2eee417369a116940fc3340a6ce538aed4a07f
龙镇人 <hljzhangzhibo@126.com>
2023-01-29 22:36:37 +0800

    docker跨架构编译修正

--- build/docker-build.sh
+++ build/docker-build.sh
@@ -2,8 +2,7 @@
 cd ../
 export DOCKER_HOST=192.168.2.205
 cd home-gateway
-# 定义变量,值为当前最后一级目录名
-docker buildx build -t registry.cn-beijing.aliyuncs.com/amur/${PWD##*/}:latest --build-arg --push --platform linux/amd64,linux/arm64 .
+docker buildx build -t registry.cn-beijing.aliyuncs.com/amur/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
 cd ../home-service
 # 遍历所有目录并切换到目录下执行docker build
 for dir in $(ls); do
@@ -13,12 +12,10 @@ for dir in $(ls); do
     cd ..
   fi
 done
-cd ../home-ops
+cd ../deploy/skywalking-ui
+docker buildx build -t registry.cn-beijing.aliyuncs.com/amur/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
+cd ../mysql
+docker buildx build -t registry.cn-beijing.aliyuncs.com/amur/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
+cd ../home-ops/home-sba
 # 遍历所有目录并切换到目录下执行docker build
-for dir in $(ls); do
-  if [ -d $dir ]; then
-    cd $dir
-    docker buildx build -t registry.cn-beijing.aliyuncs.com/amur/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
-    cd ..
-  fi
-done
+docker buildx build -t registry.cn-beijing.aliyuncs.com/amur/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
