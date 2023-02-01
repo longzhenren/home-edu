92d8897d6c19c3992e2de6931e9902c431f89a64
龙镇人 <hljzhangzhibo@126.com>
2023-02-01 10:01:40 +0800

    docker跨架构编译修正

--- build/docker-build.sh
+++ build/docker-build.sh
@@ -1,22 +1,95 @@
 #!/bin/zsh
 cd ../
-export DOCKER_HOST=192.168.2.205
-docker buildx create --use --name mybuilder
-cd home-gateway
-docker buildx build -t registry.cn-beijing.aliyuncs.com/amur/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
-cd ../home-service
-# 遍历所有目录并切换到目录下执行docker build
-for dir in $(ls); do
-  if [ -d $dir ]; then
-    cd $dir
-    docker buildx build -t registry.cn-beijing.aliyuncs.com/amur/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
-    cd ..
+# 如果环境变量中未设定DOCKER_HOST,则默认为
+if [[ -z "$DOCKER_HOST" ]]; then
+  export DOCKER_HOST=192.168.2.166
+fi
+# 如果环境变量中未设定DOCKER_REGISTRY,则默认为$DOCKER_REGISTRY
+if [[ -z "$DOCKER_REGISTRY" ]]; then
+  export DOCKER_REGISTRY=$DOCKER_REGISTRY
+fi
+docker buildx create --use
+for dir in $(find . -name Dockerfile); do
+  pwd=$(pwd)
+  if [[ $dir != *"tmp"* ]] && [[ $dir != *"target"* ]]; then
+    continue
+  fi
+  # 如果参数中有"all"
+  if [[ $@ == *"all"* ]]; then
+    cd $(dirname $dir)
+    echo Building: ${PWD##*/}
+    docker buildx build -t $DOCKER_REGISTRY/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
+    cd $pwd
+  fi
+  # 如果参数中有gateway,auth,course,msg,rtc,tinyid,user,sba,skywalking-ui,db,nacos中的一个或多个,则只构建对应目录下的Dockerfile
+  if [[ $@ == *"gateway"* ]] || [[ $@ == *"auth"* ]] || [[ $@ == *"course"* ]] || [[ $@ == *"msg"* ]] || [[ $@ == *"rtc"* ]] || [[ $@ == *"tinyid"* ]] || [[ $@ == *"user"* ]] || [[ $@ == *"sba"* ]] || [[ $@ == *"swui"* ]] || [[ $@ == *"db"* ]] || [[ $@ == *"nacos"* ]]; then
+    if [[ $dir != *"tmp"* ]] && [[ $dir != *"target"* ]]; then
+      if [[ $@ == *"gateway"* ]] && [[ $dir == *"home-gateway"* ]]; then
+        cd $(dirname $dir)
+        echo Building: ${PWD##*/}
+        docker buildx build -t $DOCKER_REGISTRY/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
+        cd $pwd
+      fi
+      if [[ $@ == *"auth"* ]] && [[ $dir == *"home-auth"* ]]; then
+        cd $(dirname $dir)
+        echo Building: ${PWD##*/}
+        docker buildx build -t $DOCKER_REGISTRY/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
+        cd $pwd
+      fi
+      if [[ $@ == *"course"* ]] && [[ $dir == *"home-course"* ]]; then
+        cd $(dirname $dir)
+        echo Building: ${PWD##*/}
+        docker buildx build -t $DOCKER_REGISTRY/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
+        cd $pwd
+      fi
+      if [[ $@ == *"msg"* ]] && [[ $dir == *"home-msg"* ]]; then
+        cd $(dirname $dir)
+        echo Building: ${PWD##*/}
+        docker buildx build -t $DOCKER_REGISTRY/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
+        cd $pwd
+      fi
+      if [[ $@ == *"rtc"* ]] && [[ $dir == *"home-rtc"* ]]; then
+        cd $(dirname $dir)
+        echo Building: ${PWD##*/}
+        docker buildx build -t $DOCKER_REGISTRY/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
+        cd $pwd
+      fi
+      if [[ $@ == *"tinyid"* ]] && [[ $dir == *"home-tinyid"* ]]; then
+        cd $(dirname $dir)
+        echo Building: ${PWD##*/}
+        docker buildx build -t $DOCKER_REGISTRY/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
+        cd $pwd
+      fi
+      if [[ $@ == *"user"* ]] && [[ $dir == *"home-user"* ]]; then
+        cd $(dirname $dir)
+        echo Building: ${PWD##*/}
+        docker buildx build -t $DOCKER_REGISTRY/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
+        cd $pwd
+      fi
+      if [[ $@ == *"sba"* ]] && [[ $dir == *"home-sba"* ]]; then
+        cd $(dirname $dir)
+        echo Building: ${PWD##*/}
+        docker buildx build -t $DOCKER_REGISTRY/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
+        cd $pwd
+      fi
+      if [[ $@ == *"skywalking-ui"* ]] && [[ $dir == *"skywalking-ui"* ]]; then
+        cd $(dirname $dir)
+        echo Building: ${PWD##*/}
+        docker buildx build -t $DOCKER_REGISTRY/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
+        cd $pwd
+      fi
+      if [[ $@ == *"db"* ]] && [[ $dir == *"db"* ]]; then
+        cd $(dirname $dir)
+        echo Building: ${PWD##*/}
+        docker buildx build -t $DOCKER_REGISTRY/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
+        cd $pwd
+      fi
+      if [[ $@ == *"nacos"* ]] && [[ $dir == *"nacos"* ]]; then
+        cd $(dirname $dir)
+        echo Building: ${PWD##*/}
+        docker buildx build -t $DOCKER_REGISTRY/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
+        cd $pwd
+      fi
+    fi
   fi
 done
-cd ../deploy/skywalking-ui
-docker buildx build -t registry.cn-beijing.aliyuncs.com/amur/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
-cd ../mysql
-docker buildx build -t registry.cn-beijing.aliyuncs.com/amur/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
-cd ../home-ops/home-sba
-# 遍历所有目录并切换到目录下执行docker build
-docker buildx build -t registry.cn-beijing.aliyuncs.com/amur/${PWD##*/}:latest --push --platform linux/amd64,linux/arm64 .
