557534fd5479b89377d613c03052ee4ba80025c5
longzhenren <longzhenren@amur.com>
2023-02-01 14:26:41 +0800

    集成auth服务;auth实体设计;config POM for api and service

---
+++ home-service-api/home-auth-api/src/main/java/com/amur/home/auth/entity/UserAuth.java
@@ -0,0 +1,43 @@
+package com.amur.home.auth.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+@Data
+@TableName(value = "user_auth", autoResultMap = true)
+public class UserAuth implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @TableId
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    @TableField(typeHandler = JacksonTypeHandler.class)
+    private Set<String> roles;
+    @TableField(typeHandler = JacksonTypeHandler.class)
+    private Set<String> permissions;
+    @TableField(fill = FieldFill.INSERT)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updateTime;
+    @Version
+    @TableField(fill = FieldFill.INSERT)
+    @JsonIgnore
+    private Integer version;
+    @TableLogic
+    @TableField(fill = FieldFill.INSERT)
+    @JsonIgnore
+    private Integer deleted;
+}
