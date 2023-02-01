c97527f6f8ba114364f61fac2dde5e5b911405ae
longzhenren <longzhenren@amur.com>
2023-02-01 14:14:14 +0800

    集成auth服务;auth服务实现

---
+++ home-service-api/home-auth-api/src/main/java/com/amur/home/auth/converter/AuthProtoConverter.java
@@ -0,0 +1,29 @@
+package com.amur.home.auth.converter;
+
+import com.amur.home.auth.entity.UserAuth;
+import com.amur.home.auth.rpc.AuthServiceProto;
+
+import java.util.HashSet;
+
+public class AuthProtoConverter {
+    public static AuthServiceProto.UserAuth toUserAuthProto(UserAuth userAuth) {
+        AuthServiceProto.UserAuth.Builder builder = AuthServiceProto.UserAuth.newBuilder()
+                .setId(userAuth.getId())
+                .setName(userAuth.getName())
+                .setPassword(userAuth.getPassword())
+                .addAllRoles(userAuth.getRoles())
+                .addAllPermissions(userAuth.getPermissions());
+
+        return builder.build();
+    }
+
+    public static UserAuth toUserAuth(AuthServiceProto.UserAuth proto) {
+        UserAuth userAuth = new UserAuth();
+        userAuth.setId(proto.getId());
+        userAuth.setName(proto.getName());
+        userAuth.setPassword(proto.getPassword());
+        userAuth.setRoles(new HashSet<>(proto.getRolesList()));
+        userAuth.setPermissions(new HashSet<>(proto.getPermissionsList()));
+        return userAuth;
+    }
+}
