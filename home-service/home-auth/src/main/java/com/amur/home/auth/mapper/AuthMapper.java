c650bc0e488091f3601be6b024bc60bffb051143
longzhenren <longzhenren@amur.com>
2023-02-02 16:12:38 +0800

    auth服务实现

---
+++ home-service/home-auth/src/main/java/com/amur/home/auth/mapper/AuthMapper.java
@@ -0,0 +1,11 @@
+package com.amur.home.auth.mapper;
+
+import com.amur.home.auth.entity.UserAuth;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+
+@Mapper
+public interface AuthMapper extends BaseMapper<UserAuth> {
+}
