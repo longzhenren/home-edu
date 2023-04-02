20405967796b6afe966ab85becbd84b012665a76
longzhenren <longzhenren@amur.com>
2023-04-02 21:33:00 +0800

    管理后台服务实现;integrate springboot admin

---
+++ home-admin/src/main/java/com/amur/home/admin/mapper/ChatInviteMapper.java
@@ -0,0 +1,9 @@
+package com.amur.home.admin.mapper;
+
+import com.amur.home.msg.entity.ChatInvite;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ChatInviteMapper extends BaseMapper<ChatInvite> {
+}
