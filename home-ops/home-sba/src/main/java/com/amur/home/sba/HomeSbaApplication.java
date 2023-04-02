efb57823208c4da89c35492428985b95a5bb3c39
longzhenren <longzhenren@amur.com>
2023-04-02 19:46:42 +0800

    添加管理后台;管理后台服务实现;添加管理后台配置

---
+++ home-ops/home-sba/src/main/java/com/amur/home/sba/HomeSbaApplication.java
@@ -0,0 +1,15 @@
+package com.amur.home.sba;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.amur.home"})
+@EnableAdminServer
+public class HomeSbaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeSbaApplication.class, args);
+    }
+
+}
