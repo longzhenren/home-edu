39adc3634e60e9d5adc597a4112523ce6bec8d1c
longzhenren <longzhenren@amur.com>
2023-04-04 16:09:27 +0800

    管理后台服务实现

---
+++ home-admin/src/main/java/com/amur/home/admin/mapper/CourseScoreMapper.java
@@ -0,0 +1,10 @@
+package com.amur.home.admin.mapper;
+
+import com.amur.home.course.entity.CourseScore;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface CourseScoreMapper extends BaseMapper<CourseScore> {
+}
