750573d282faacab56830cc7352d823d578c0e9c
longzhenren <longzhenren@amur.com>
2023-04-05 14:11:56 +0800

    管理后台服务实现;添加管理后台

---
+++ home-admin/src/main/java/com/amur/home/admin/mapper/CourseCommentMapper.java
@@ -0,0 +1,10 @@
+package com.amur.home.admin.mapper;
+
+import com.amur.home.course.entity.CourseComment;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface CourseCommentMapper extends BaseMapper<CourseComment> {
+}
