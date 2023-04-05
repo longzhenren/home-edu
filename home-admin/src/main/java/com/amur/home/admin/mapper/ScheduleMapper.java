8873432d847f120e277403e3b4571d89118dcc75
longzhenren <longzhenren@amur.com>
2023-04-05 22:54:12 +0800

    管理后台服务实现;添加管理后台配置

---
+++ home-admin/src/main/java/com/amur/home/admin/mapper/ScheduleMapper.java
@@ -0,0 +1,9 @@
+package com.amur.home.admin.mapper;
+
+import com.amur.home.schedule.entity.Schedule;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ScheduleMapper extends BaseMapper<Schedule> {
+}
---
+++ home-admin/src/main/java/com/amur/home/admin/service/impl/HomeInfoServiceImpl.java
@@ -0,0 +1,26 @@
+package com.amur.home.admin.service.impl;
+
+import com.amur.home.admin.mapper.HomeInfoMapper;
+import com.amur.home.admin.service.HomeInfoService;
+import com.amur.home.user.entity.HomeInfo;
+import com.amur.home.util.QueryUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+@Service
+public class HomeInfoServiceImpl extends ServiceImpl<HomeInfoMapper, HomeInfo> implements HomeInfoService {
+    @Resource
+    private HomeInfoMapper HomeInfoMapper;
+
+    @Override
+    public Page<HomeInfo> queryHomeInfos(Map<String, Map<String, Object>> params, String sortField, String sortOrder, Integer pageNumber, Integer pageSize) {
+        QueryWrapper<HomeInfo> queryWrapper = QueryUtil.buildQueryWrapper(params, sortField, sortOrder);
+        Page<HomeInfo> page = new Page<>(pageNumber, pageSize);
+        return HomeInfoMapper.selectPage(page, queryWrapper);
+    }
+}
