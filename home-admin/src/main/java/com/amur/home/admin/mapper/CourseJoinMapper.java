86b8729dafefa74790cbea2a8c0055c10afd2df0
longzhenren <longzhenren@amur.com>
2023-04-05 23:29:06 +0800

    管理后台服务实现;添加管理后台

---
+++ home-admin/src/main/java/com/amur/home/admin/mapper/CourseJoinMapper.java
@@ -0,0 +1,7 @@
+package com.amur.home.admin.mapper;
+
+import com.amur.home.course.entity.CourseJoinRelation;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface CourseJoinMapper extends BaseMapper<CourseJoinRelation> {
+}
